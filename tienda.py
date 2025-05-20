b11ab35049f2ec07bf60a633ef52177317586354
Paul Guerra <paulmartinguerrayaicate@gmail.com>
2025-05-20 13:22:01 -0500

    Main class

--- tienda.py
+++ tienda.py
@@ -1,2 +1,20 @@
 import tkinter as tk 
 from tkinter import ttk, messagebox
+
+# Clase principal de la aplicación
+class MusicStoreApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tienda de la Música")
+        self.geometry("500x400")
+        self.resizable(False, False)
+        
+        # Contenedor principal
+        self.container = tk.Frame(self)
+        self.container.pack(fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        self.usuario = ""
+        self.datos_compra = {}
