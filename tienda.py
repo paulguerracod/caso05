05a5d819a6c55ad0c5a0f191ef2e9bfa34f63fb2
Paul Guerra <paulmartinguerrayaicate@gmail.com>
2025-05-20 13:31:10 -0500

    Create all frames

--- tienda.py
+++ tienda.py
@@ -18,3 +18,19 @@ class MusicStoreApp(tk.Tk):
         self.frames = {}
         self.usuario = ""
         self.datos_compra = {}
+
+        # Crear todos los frames
+        for F in (WelcomeFrame, DataEntryFrame, ResultsFrame, CreditsFrame):
+            frame = F(self.container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(WelcomeFrame)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        if cont == ResultsFrame:
+            frame.actualizar_resultados()
+        elif cont == CreditsFrame:
+            frame.actualizar_creditos()
